b8d25c9bd30166d0dfa29044bc42810aec80d271
carmenlogue <carmenlogue@gmail.com>
2018-10-19 11:41:06 +0100

    Add gem devise and User model

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'carrierwave'
+gem 'devise'
 gem 'dotenv-rails'
 
 group :development, :test do
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
+
+  validates :email, presence: true
+end
