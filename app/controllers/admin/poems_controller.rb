00390e5a01dd1e46f21dfb132f3a3a6e8d21d845
carmenlogue <carmenlogue@gmail.com>
2018-10-22 15:58:10 +0100

    Poems admin management and gem kaminari

--- app/controllers/admin/poems_controller.rb
+++ app/controllers/admin/poems_controller.rb
@@ -1,7 +1,61 @@
 module Admin
   class PoemsController < AdminController
+    before_action :set_poem, only: [:show, :edit, :update, :destroy]
+
     def index
-      @poems = Poem.all.order(:fragment)
+      results = if params[:query]
+                  Poem.all.order(:fragment).where(
+                    'lower(fragment) LIKE ?',
+                    "%#{params[:query].downcase}%",
+                  )
+                else
+                  Poem.all.order(:fragment)
+                end
+      @poems = results.page(params[:page])
+    end
+
+    def new
+      @poem = Poem.new
+    end
+
+    def create
+      @poem = Poem.new(poem_params)
+      if @poem.save
+        redirect_to success_path, notice: 'Successfully created poem.'
+      else
+        render :new
+      end
+    end
+
+    def show; end
+
+    def edit; end
+
+    def update
+      if @poem.update(poem_params)
+        redirect_to success_path, notice: 'Successfully updated poem.'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @poem.destroy
+      redirect_to admin_poems_path, notice: 'Successfully deleted poem.'
+    end
+
+    private
+
+    def set_poem
+      @poem = Poem.find(params[:id])
+    end
+
+    def poem_params
+      params[:poem].permit(:fragment)
+    end
+
+    def success_path
+      admin_poem_path(@poem)
     end
   end
 end
--- spec/models/poem_spec.rb
+++ spec/models/poem_spec.rb
@@ -5,6 +5,5 @@ RSpec.describe Poem, type: :model do
   it { is_expected.to(have_and_belong_to_many(:categories)) }
   it { is_expected.to(belong_to(:song)) }
 
-  it { is_expected.to validate_presence_of(:reference) }
   it { is_expected.to validate_uniqueness_of(:reference) }
 end
