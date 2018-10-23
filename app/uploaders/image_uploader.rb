adb6002e7b66762c0071303affc9dbab24bc7486
carmenlogue <carmenlogue@gmail.com>
2018-10-23 11:45:08 +0100

    Add images uploader for poems

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,15 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w[jpg jpeg gif png svg]
+  end
+
+  version :admin_thumb do
+    process resize_to_limit: [200, 200]
+  end
+end
